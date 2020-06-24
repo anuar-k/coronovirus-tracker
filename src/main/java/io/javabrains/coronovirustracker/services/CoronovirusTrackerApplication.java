66c87efceaf0f98b46edc542ef22432fb2cbd09a
Anuar <https://github.com/anuar-k/HTML_CSS_2>
2020-06-24 20:08:59 +0600

    init commit

---
+++ src/main/java/io/javabrains/coronovirustracker/services/CoronovirusTrackerApplication.java
@@ -0,0 +1,15 @@
+package io.javabrains.coronovirustracker.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronovirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronovirusTrackerApplication.class, args);
+	}
+
+}
